3b97db8e169f855a616039167d7e86d2a6fd4c9d
Ninti357 <diegoandresmr1403@gmail.com>
2024-10-03 14:43:10 -0400

    fix registro users

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -23,16 +23,7 @@ class UserResource extends Resource
     {
         return $form
             ->schema([
-                Forms\Components\TextInput::make('name')
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('email')
-                    ->email()
-                    ->required()
-                    ->maxLength(255),
-                Forms\Components\TextInput::make('cedula')
-                    ->required()
-                    ->maxLength(255),
+               
                 Forms\Components\TextInput::make('primer_nombre')
                     ->required()
                     ->maxLength(255),
@@ -43,7 +34,14 @@ class UserResource extends Resource
                     ->maxLength(255),
                 Forms\Components\TextInput::make('segundo_apellido')
                     ->maxLength(255),
-                Forms\Components\DateTimePicker::make('email_verified_at'),
+                    Forms\Components\TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('cedula')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\DatePicker::make('fecha_de_nacimiento'),
                 Forms\Components\TextInput::make('password')
                     ->password()
                     ->required()
