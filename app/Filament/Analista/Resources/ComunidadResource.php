ee7f21d34cbf008ae4ae9614eec78d865a7351a5
Ninti357 <diegoandresmr1403@gmail.com>
2024-10-30 16:48:13 -0400

    fix pannel analista

--- app/Filament/Analista/Resources/ComunidadResource.php
+++ app/Filament/Analista/Resources/ComunidadResource.php
@@ -19,7 +19,7 @@ class ComunidadResource extends Resource
 
     protected static ?string $navigationLabel = 'Comunidades';
     protected static ?string $label = 'Comunidades';
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-s-home';
 
     public static function form(Form $form): Form
     {
