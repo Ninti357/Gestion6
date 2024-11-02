f1b25a063a6d3b34aad8d3f82cbbeb2a74c4bb07
Ninti357 <diegoandresmr1403@gmail.com>
2024-11-02 13:25:19 -0400

    add login image

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Filament\Support\Facades\FilamentView;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        FilamentView::registerRenderHook(
+                'panels::auth.login.form.after',
+            fn (): string => Blade::render('@vite(\'resources/css/custom-login.css\')'),
+        );
     }
 }
